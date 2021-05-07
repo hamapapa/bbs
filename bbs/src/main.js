de974b6a138c594fd84e6000d6bb5bacb64120b3
hamapapa <smile.taiyo8686@gmail.com>
2021-05-07 19:52:04 +0900

    axiosでgetリクエストを送ることはできた

--- bbs/src/main.js
+++ bbs/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(store).use(router).mount('#app')
+
+// axios.defaults.baseURL = "http://localhost";
+// axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+// axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
+createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
--- bbs/src/views/User.vue
+++ bbs/src/views/User.vue
@@ -2,4 +2,37 @@
   <div class="p-5 text-center">
     <h1>ユーザリスト</h1>
   </div>
+  <div class="text-center">
+    {{ users }}
+  </div>
 </template>
+
+<script>
+// import { computed, defineComponent } from "vue";
+import { defineComponent, reactive, toRefs, watchEffect } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      users: "",
+    });
+
+    watchEffect(() => {
+      axios
+        .get("http://localhost:8080/")
+        .then((response) => {
+          state.users = response.data;
+        })
+        .catch((e) => {
+          state.users = "no user";
+          console.log(e);
+        });
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
