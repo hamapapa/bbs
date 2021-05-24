719f7b4f33dfa850ad8553bf470ef647bacacb90
hamapapa <smile.taiyo8686@gmail.com>
2021-05-24 18:34:34 +0900

    laravel passport を利用してログイン、ログアウトできるようにする

--- bbs/src/store/index.js
+++ bbs/src/store/index.js
@@ -2,8 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    isLogined: false,
+    userName: "",
+    token: ""
   },
   mutations: {
+    login(state, payload) {
+      state.token = payload.token;
+      state.userName = payload.userName;
+      state.isLogined = true;
+    },
+
+    logout(state) {
+      state.token = "";
+      state.userName = "";
+      state.isLogined = false;
+    }
   },
   actions: {
   },
